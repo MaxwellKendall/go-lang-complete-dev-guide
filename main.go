c30e1173ed5ed2967c00ec8c42812740c8c691b5
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-12-19 17:29:44 -0500

    clarifying the OO approach comment

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 // Go is not Object Oriented, no "Deck Class" with defined classes that have certain methods and properties
-// instead, we create a new type, similar to strings, ints, slices, and arrays
+// but this is vaugely like the OO approach, we create a new type, similar to strings, ints, slices, and arrays
 // execute go run main.go deck.go
 package main
 
