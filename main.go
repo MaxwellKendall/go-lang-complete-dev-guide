14b844ced33ceaa027c62f100bbb6db0f477ed2b
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-12-19 16:48:18 -0500

    ranging over the slice!

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ func main() {
 	cards := []string{newCard(), newCard(), "Ace of Diamonds"}
 	// actually creates a new slice!
 	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	
 	fmt.Println(cards)
 }
 
