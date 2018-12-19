07e23d087a7f41377ef3557f1e50b5db8006e025
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-12-19 17:55:50 -0500

    removing comments from previous branch

--- main.go
+++ main.go
@@ -1,13 +1,3 @@
-// accessing slices:
-
-// slices are indexed from 0
-// acessing a slice like so slice[0]
-// selecting a range of items in a slice: `slice[index1:index2]`
-//  - index1 is inclusive, index2 is exclusive
-
-// `slice[:index2]` items up to and excluding index2
-// `slice[index2:]` items begining at and including index2, to the end 
-
 package main
 
 func main() {
