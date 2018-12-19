b2e649778b9ed1181706f245b4ba321f9fe926a8
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-12-19 17:24:48 -0500

    successfully created print method on type deck

---
+++ deck.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Create a new type of deck which is a slice of string
+
+type deck []string
+
+func (d deck) print() {
+		for i, card := range d {
+		// every iteration, we create a new i and card variable
+		fmt.Println(i, card)
+	}
+}
--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 // Go is not Object Oriented, no "Deck Class" with defined classes that have certain methods and properties
 // instead, we create a new type, similar to strings, ints, slices, and arrays
+// execute go run main.go deck.go
 package main
 
-import "fmt"
-
 func main() {
-	// var card string = "Ace of Spades" // only strings are legit!
-	cards := []string{newCard(), newCard(), "Ace of Diamonds"}
-	// actually creates a new slice!
+	cards := deck{newCard(), newCard(), "Ace of Diamonds"}
+
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		// every iteration, we create a new i and card variable
-		fmt.Println(i, card)
-	}
-
-	fmt.Println(cards)
+	cards.print()
 }
 
 func newCard() string {
