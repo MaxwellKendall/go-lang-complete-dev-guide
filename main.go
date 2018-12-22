9aed941fa74af0f73b59b4529270035e0240b68e
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-22 15:14:23 -0500

    adding git ignore and showing type coercion

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Usinge the go standard library we often need the type 'byte slice' which is a computer friendly way of representing a string
 // See this table: http://www.asciitable.com/ (Bytes are referred to here as decimals in the table)
 
 func main() {
 	// execute go run main.go deck.go
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+	definitelyAString := "This is most definitely a string"
+	fmt.Println([]byte(definitelyAString))
 }
