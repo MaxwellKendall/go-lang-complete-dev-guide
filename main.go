9e42671b0eae26951abffe37f3b5ede83b4d7eae
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-22 14:09:36 -0500

    simple comment about byte slices in main.go

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+// Usinge the go standard library we often need the type 'byte slice' which is a computer friendly way of representing a string
+// See this table: http://www.asciitable.com/ (Bytes are referred to here as decimals in the table)
+
 func main() {
 	// execute go run main.go deck.go
 	cards := newDeck()
