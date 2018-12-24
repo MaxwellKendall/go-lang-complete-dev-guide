c59ab25e028497b2892c8a16e8ff76839e293110
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-24 10:44:45 -0500

    implementing i/o testing

--- dec_test.go
+++ dec_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -11,4 +12,24 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected length of 52 but got %v", len(d))
 	}
 
+	// first card is ace of spades
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades but got %v", d[0])
+	}
+	// last card is the king of diamonds
+	if d[len(d)-1] != "King of Diamonds" {
+		t.Errorf("Expected last card to be King of Diamonds but got %v", d[len(d)-1])
+	}
+}
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck, _ := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected loaded deck to be of length 52 instead got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
