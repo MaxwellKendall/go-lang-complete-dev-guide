cff50dba1a7e847f18f877f0a311089490360f46
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-24 11:15:03 -0500

    init and updating structs

--- structs/main.go
+++ structs/main.go
@@ -8,14 +8,19 @@ import (
 
 type person struct {
 	firstName string
-	lsatName string
+	lastName string
 }
 
 func main() {
 	// relying on the order of definition
 	max := person{"Max", "Kendall"}
 	// explicitly specifying the properties and giving them values
-	claire := person{firstName: "Claire", lsatName: "Kendall"}
+	claire := person{firstName: "Claire", lastName: "Kendall"}
+	// init empty struct with 0 values
+	var ross person
+	// updating new person using . syntax
+	claire.lastName = "SQRL"
 
-	fmt.Println(max, claire)
+	fmt.Println(max, claire, ross)
+	fmt.Printf("%+v", ross) // shows all the fields!
 }
