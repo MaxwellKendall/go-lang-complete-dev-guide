03d3cf588b868971ec626b1f3af0398fb11a4bcc
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-27 13:24:58 -0500

    a problem scenario where interfaces can be used as a solution

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,5 +1,35 @@
+// Interfaces will help us reuse the printGreeting accross multiple types
+
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
 }
+
+type spanishBot struct{}
+type englishBot struct{}
+
+func (englishBot) getGreeting() string {
+	// very custom logic for english greeting
+	return "Hello there"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
