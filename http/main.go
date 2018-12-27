a671300fe3c572e3180f3210a4bff35fcfa67e98
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-27 14:51:01 -0500

    implementing a custom writer

--- http/main.go
+++ http/main.go
@@ -7,14 +7,22 @@ import (
 	"net/http"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	// io.Copy accepts 2 params: (1) implements Writer if, (2) Implements Reader if
-	// os.Stdout is of type file, which implements Writer, which therefore satisfies (1)
-	// resp.Body implements the Read() fn which satisfies (2)
-	io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (l logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote", len(bs), "bytes")
+	return len(bs), nil
 }
