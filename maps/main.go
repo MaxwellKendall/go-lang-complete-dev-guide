eeda20438398d0414f90a592119f428e85b4c1a7
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-27 12:57:32 -0500

    adding values to a map

--- maps/main.go
+++ maps/main.go
@@ -6,10 +6,16 @@ import (
 
 func main() {
 	colors := make(map[string]string)
+
 	// var colors map[string]string
 	// colors := map[string]string{
 	// 	"red": "#ff000",
 	// 	"green": "#745",
 	// }
+
+	colors["white"] = "#ffff"
+	// dot syntax (colors.white = "XYZ") does not work because key has to be of a certain type
+	delete(colors, "white")
+
 	fmt.Println(colors)
 }
